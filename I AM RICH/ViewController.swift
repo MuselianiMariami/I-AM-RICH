a9669c4eb93a0c698085c1b0252fdbc3d3691c53
MuselianiMariami <museliani61@gmail.com>
2024-04-10 17:27:18 +0400

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Mariam Museliani on 24.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
